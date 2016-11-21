9be220a46610050ad2d531fe274f3e58e9dbede4
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2016-11-21 11:02:57 +0100

    Basic twitter reader

---
+++ twit.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import random
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = os.getenv("TWITTER_ACCESS_KEY")
+access_token_secret = os.getenv("TWITTER_SECRET_KEY")
+consumer_key = os.getenv("TWITTER_C_ACCESS_KEY")
+consumer_secret = os.getenv("TWITTER_C_SECRET_KEY")
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+
+        textings = json.loads(data)['text']
+        sampl = textings.split()
+
+
+        print  '%s%s%s' % ('\t'*random.randint(1,9),random.choice(sampl),'\n'*random.randint(1,3)) 
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    data = stream.filter(track=['Pandas','Panda','pandas','panda'])
