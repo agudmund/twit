2d9aa153f79bab73232f0d07786ec09d798144b4
beenormal <aevar.gudmundsson@gmail.com>
2017-05-06 19:29:34 +0200

    Base listener

---
+++ listen.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# *-* coding: utf-8 *-* 
+
+import os
+import sys
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy import API
+
+
+
+class StdOutListener(StreamListener):
+
+
+    def on_data(self, data):
+
+        result = json.loads(data)
+        try:
+            print result['text']
+        except KeyError as e:
+            return True
+
+        return True
+
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+
+    access_token = os.getenv("TWITTER_ACCESS_KEY")
+    access_token_secret = os.getenv("TWITTER_SECRET_KEY")
+    consumer_key = os.getenv("TWITTER_C_ACCESS_KEY")
+    consumer_secret = os.getenv("TWITTER_C_SECRET_KEY")
+
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret )
+    stream = Stream(auth, l)
+
+    data = stream.filter(track=sys.argv[-1])
